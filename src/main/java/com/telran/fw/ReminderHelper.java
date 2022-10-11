b5a51e022532737dff9c07b10545924d78930483
RodionGlezerov <107550494+RodionGlezerov@users.noreply.github.com>
2022-10-11 23:41:42 +0300

    Added class "addReminderWithRandomSwipeTestRepeat"

--- src/main/java/com/telran/fw/ReminderHelper.java
+++ src/main/java/com/telran/fw/ReminderHelper.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 
 import java.util.List;
 
-public class ReminderHelper extends HelperBase{
+public class ReminderHelper extends HelperBase {
 
 
     public ReminderHelper(AppiumDriver driver) {
@@ -14,17 +14,17 @@ public class ReminderHelper extends HelperBase{
     }
 
 
-    public void tapOnAddButton(){
+    public void tapOnAddButton() {
         click(By.id("add_reminder"));
     }
 
     public void fillReminderName(String text, int timeOut) {
-        waitForElementAndType(By.id("reminder_title"),timeOut,text);
+        waitForElementAndType(By.id("reminder_title"), timeOut, text);
         hideKeyboard();
     }
 
     public void saveReminder(int timeOut) {
-        waitForElementAndClick(By.xpath("//android.widget.TextView[@content-desc='Save Reminder']"),timeOut);
+        waitForElementAndClick(By.xpath("//android.widget.TextView[@content-desc='Save Reminder']"), timeOut);
     }
 
     public void tapOnData() {
@@ -38,5 +38,33 @@ public class ReminderHelper extends HelperBase{
 
     public void tapOnOk() {
         click(By.id("ok"));
+    }
+
+
+    public void selectRepeat() {
+        click(By.id("repeat_switch"));
+    }
+
+    public void selectRepeatitionInterval() {
+        click(By.id("RepeatNo"));
+    }
+
+    public void fillRepeatitionInterval(String text, int timeOut) {
+        waitForElementAndType(By.className("android.widget.EditText"), timeOut, text);
+        hideKeyboard();
+    }
+    public void tapOnOkRepetitionInterval() {
+        click(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.Button[2]"));
+    }
+
+    public void typeOfRepetition(){
+        click(By.id("RepeatType"));
+    }
+
+    public void ckickDayRepetition(){
+        click(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.ListView/android.widget.TextView[3]"));
+    }
+    public void clickSaveReminder(){
+        click(By.id("save_reminder"));
     }
 }
--- src/test/java/com/telran/remindly/tests/CreateReminderTests.java
+++ src/test/java/com/telran/remindly/tests/CreateReminderTests.java
@@ -18,7 +18,7 @@ public class CreateReminderTests extends TestBase{
         Assert.assertEquals(quantityAfterAdd, quantityBeforeAdd +1);
     }
 
-    @Test
+    @Test (enabled = false)
     public void addReminderWithRandomSwipeTest(){
 
         app.getReminder().tapOnAddButton();
@@ -31,6 +31,25 @@ public class CreateReminderTests extends TestBase{
         app.getReminder().tapOnOk();
         app.getReminder().saveReminder(20);
         app.getReminder().pause(4000);
+    }
+
+    @Test
+    public void addReminderWithRandomSwipeTestRepeat(){
+        app.getReminder().tapOnAddButton();
+        app.getReminder().fillReminderName("Test",20);
+        app.getReminder().pause(3000);
+        app.getReminder().selectRepeat();
+        app.getReminder().selectRepeatitionInterval();
+        app.getReminder().pause(1000);
+        app.getReminder().fillRepeatitionInterval("2",20);
+        app.getReminder().pause(2000);
+        app.getReminder().tapOnOkRepetitionInterval();
+        app.getReminder().swipeUp();
+        app.getReminder().typeOfRepetition();
+        app.getReminder().ckickDayRepetition();
+        app.getReminder().clickSaveReminder();
+
+
     }
 
 
